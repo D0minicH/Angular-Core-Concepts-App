696cb16cdce743b78b72152d6922e0365cdb1d06
Dominic Hein <dominic.hein@students.fhnw.ch>
2019-11-03 18:35:31 +0100

    All components & modules static skeleton finished

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularCoreConceptsApp
+# Angular Core Concepts App
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.17.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CustomersModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, AppRoutingModule, CustomersModule, SharedModule ],
+  declarations: [ AppComponent ],
+  providers:    [],
+  bootstrap:    [AppComponent]
 })
 export class AppModule { }
