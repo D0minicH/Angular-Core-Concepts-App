359e3c4be491a1b296dd8f2ceb32a4eec27b2c63
Dominic Hein <dominic.hein@students.fhnw.ch>
2019-11-07 20:02:30 +0100

    Routing: Orders-view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrdersComponent } from './orders/orders.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // Redirect in case you call the startpage
+  { path: '', pathMatch: 'full', redirectTo: '/customers'},
+
+  // Alternative to standalone feature-routing file, apparently not working because of order, why?
+  { path: 'orders/:id', component: OrdersComponent },
+
+  // Redirect in case you call a non-existing path
+  { path: '**', pathMatch: 'full', redirectTo: '/customers' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
